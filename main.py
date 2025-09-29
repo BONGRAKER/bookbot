ab74df435424e091a94219a2173b2aa917003b29
BONGRAKER <smileh@gmail.com>
2025-09-29 17:44:46 +0300

    Added check for file input, dynamic file path accept

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import count_characters
 from stats import sort_char_dict
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,7 +10,14 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    # Check for command line argument
+    if sys.argv and len(sys.argv) > 1:
+        book_path = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     num_characters = count_characters(book_text, {})
     sorted_characters = sort_char_dict(num_characters)
