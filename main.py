8d1a70b490144ddda14608a84a185d6a5b9019b7
BONGRAKER <smileh@gmail.com>
2025-09-29 17:39:01 +0300

    Added sorted list of letter count as output

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import get_num_words
+from stats import count_characters
+from stats import sort_char_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,7 +11,18 @@ def get_book_text(filepath):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book_text)
+    num_characters = count_characters(book_text, {})
+    sorted_characters = sort_char_dict(num_characters)
+    
+    print("================BOOKBOT===================")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------------Word Count------------------")
     print(f"Found {num_words} total words")
+    print("---------------Character Count--------------")
+    for char, count in sorted_characters:
+        print(f"{char}: {count}")
+    print("==================END=====================")
+    
 
 if __name__ == "__main__":
     main()
